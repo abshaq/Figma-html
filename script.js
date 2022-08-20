151bea5106ddb0008b97d97b6f64684ec26daf33
Shiraz Syed Qazi <110056843+abshaq@users.noreply.github.com>
2022-08-20 16:19:20 +0400

    completed the figma to html project

---
+++ script.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector('.nav');
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+
+
+open.addEventListener('click', () => {
+    nav.style.display = 'block';
+    open.classList.add('hidden');
+    close.classList.remove('hidden');
+})
+
+close.addEventListener('click', () => {
+    nav.style.display = 'none';
+    open.classList.remove('hidden');
+    close.classList.add('hidden');
+})
